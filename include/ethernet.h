f0c4bfb616f4c76abf6a331729f5e8b6baeceb37
MaciejGit6 <macie.milewski@gmail.com>
2026-05-20 02:16:10 +0200

    ethernet.c implemented

--- include/ethernet.h
+++ include/ethernet.h
@@ -1,5 +1,8 @@
 #pragma once 
 #include <stdint.h>
+#include <stddef.h>
+#include <sys/types.h>
+
 
 #define ETHERNETYPE_IPV4 0x0800
 #define ETHERNETYPE_IPV6 0x86DD
@@ -8,6 +11,10 @@
 
 #define ETH_ALEN 6
 #define ETH_HLEN 14
+
+void mac_to_str (const uint8_t* mac, char* out, size_t out_len);
+const uint8_t* parse_ethernet(const uint8_t* buf, ssize_t len, char* src_mac,
+    char* dst_mac,  uint16_t* ethertype);
 
 
 typedef struct{
--- src/ethernet.c
+++ src/ethernet.c
@@ -1,11 +1,24 @@
+#include "ethernet.h"
+#include <string.h>
+#include <stdio.h>
+#include <arpa/inet.h>
 
-void mac_to_str(const uint8_t* mac, char* out, size_t out_len);
+void mac_to_str(const uint8_t* mac, char* out, size_t out_len){
+    snprintf(out, out_len, "%02x:%02x:%02x:%02x:%02x:%02x",
+    mac[0], mac[1], mac[2], mac[4], mac[5]);
+}
 
 const uint8_t* parse_ethernet(const uint8_t* buf, ssize_t len, char* src_mac, char* dst_mac, uint16_t* ethertype){
     if(len < ETH_HLEN){
         return NULL;
     }
-    eth_header_t* eth_head = (eth_header_t*)buf;
+    const eth_header_t* eth = (const eth_header_t*)buf;
+
+    mac_to_str(eth->src, src_mac, 18);
+    mac_to_str(eth->dst, dst_mac, 18);
+    *ethertype = ntohs(eth->etherntype);
+
+    return buf + ETH_HLEN;
     
 }
 
